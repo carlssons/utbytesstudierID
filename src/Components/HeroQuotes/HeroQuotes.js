80c3331aadc0ff095e406ff66e00351980151546
Hanna Andersson <hannaandersson010@gmail.com>
2018-11-28 15:23:45 +0100

    created hero-quote

---
+++ src/Components/HeroQuotes/HeroQuotes.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./HeroQuotes.scss";
+
+class HeroQuotes extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="quotes">
+        <img
+          className="quote-image"
+          src={"./images/" + this.props.content.img}
+          alt="quoteImage"
+        />
+        <div className="quote-text">
+          <p id="quote">{this.props.content.quote}</p>
+          <p id="quote-info">{this.props.content.info}</p>
+          <Link to="/intervjuer" id="interview-link">
+            Läs intervju
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HeroQuotes;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,52 @@
 import React, { Component } from "react";
 import "./Home.scss";
 import HeroSection from "../HeroSection/HeroSection.js";
+import HeroQuotes from "../HeroQuotes/HeroQuotes.js";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      quotes: [
+        {
+          img: "Linus.jpg",
+          quote:
+            "“Många spännande och intressanta kurser man inte får möjlighet att läsa i Umeå”",
+          info: "- Linus Lagerhjelm, Tjeckien"
+        },
+        {
+          img: "Amine.jpg",
+          quote:
+            "“Många spännande och intressanta kurser man inte får möjlighet att läsa i Umeå”",
+          info: "- Hanna Andersson, Singapore"
+        },
+        {
+          img: "Viktor.jpg",
+          quote: "“Tiden i Kanada är nog den tid jag växt mest som person”",
+          info: "- Viktor Ålund, Winnipeg Kanada"
+        },
+        {
+          img: "Amine.jpg",
+          quote:
+            "”Att uppleva nya kulturer är så berikande i både erfarenheter och perspektivet man har på livet”",
+          info: "- Amine Balta, Seoul Sydkorea"
+        }
+      ]
+    };
   }
 
   render() {
     return (
-
+      <div>
         <HeroSection />
-      
+        <div className="quote-container">
+          <div className="quote-box">
+            {this.state.quotes.map((element, index) => {
+              return <HeroQuotes key={index} content={element} />;
+            })}
+          </div>
+        </div>
+      </div>
     );
   }
 }
