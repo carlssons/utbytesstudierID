e1f2cef7a10fe42242ded3dccc3e559fdd023648
Hanna Andersson <hannaandersson010@gmail.com>
2018-11-29 16:13:28 +0100

    indentate

--- src/Components/SideDrawer/SideDrawer.js
+++ src/Components/SideDrawer/SideDrawer.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import './SideDrawer.scss';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./SideDrawer.scss";
+import { Link } from "react-router-dom";
 
-const SideDrawer = props =>{
+const SideDrawer = props => {
+  let drawerClasses = "side-drawer";
 
-  let drawerClasses ='side-drawer';
-
-  if (props.show){
-    drawerClasses = 'side-drawer open';
+  if (props.show) {
+    drawerClasses = "side-drawer open";
   }
 
-  return(
+  return (
     <nav className={drawerClasses}>
       <ul>
-         <li><Link className="navLink" to="/">Hem</Link></li>
-         <li><Link to="/intervjuer">Intervjuer</Link></li>
-         <li><Link className="navLink" to="/avtal">Avtal</Link></li>
-         <li><Link className="navLink" to="/efterResan">Efter resan</Link></li>
-         <li><Link className="navLink" to="/faq">FAQ</Link></li>
+        <li>
+          <Link className="navLink" to="/">
+            Hem
+          </Link>
+        </li>
+        <li>
+          <Link to="/intervjuer">Intervjuer</Link>
+        </li>
+        <li>
+          <Link className="navLink" to="/avtal">
+            Avtal
+          </Link>
+        </li>
+        <li>
+          <Link className="navLink" to="/efterResan">
+            Efter resan
+          </Link>
+        </li>
+        <li>
+          <Link className="navLink" to="/faq">
+            FAQ
+          </Link>
+        </li>
       </ul>
     </nav>
   );
-
 };
 
 export default SideDrawer;
