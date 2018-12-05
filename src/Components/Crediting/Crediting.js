40f0171673b2ebc04aaf3b5c24f20c7001c70d1f
Hanna Andersson <hannaandersson010@gmail.com>
2018-12-05 10:24:56 +0100

    changed json in crediting

--- src/Components/Crediting/Crediting.js
+++ src/Components/Crediting/Crediting.js
@@ -6,29 +6,18 @@ class Crediting extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cover: [
-        {
-          backgroundImg: "heroImageCrediting",
-          title: "INTERVJUER",
-          subtitle:
-            "ID-studenter har varit på utbytesstudier på flera olika partneruniversitet runt om i världen. Intervjuerna innehåller  allt ifrån tips till praktiska grejer inför resan från några IDare som har varit iväg."
-        }
-      ],
-      coverText: [
-        {
-          title: "TILLGODORÄKNANDE",
-          subtitle:
-            "De kurser som du läser utomlands behöver tillgodoräknas in i dina studier på ID-programmet. På den här sidan kommer det finnas en lista på kurser som andra har läst vid olika partneruniversitet under deras utlandsstudier. Det kommer även stå vad varje kurs tillgodoräknades som."
-        }
-      ]
+      cover: {
+        backgroundImg: "heroImageCrediting",
+        title: "TILLGODORÄKNANDE",
+        subtitle:
+          "ID-studenter har varit på utbytesstudier på flera olika partneruniversitet runt om i världen. Intervjuerna innehåller  allt ifrån tips till praktiska grejer inför resan från några IDare som har varit iväg."
+      }
     };
   }
   render() {
     return (
-      <div className="cover-crediting">
-        {this.state.cover.map((element, index) => {
-          return <Cover key={index} content={element} />;
-        })}
+      <div>
+        <Cover content={this.state.cover} />
       </div>
     );
   }
