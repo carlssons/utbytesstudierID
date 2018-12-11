81596d87396214e63bd649d7ad80ce38ed487492
Hanna Andersson <hannaandersson010@gmail.com>
2018-12-11 13:17:26 +0100

    fixed crediting

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,53 +4,51 @@ import "./Navbar.scss";
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton.js";
 
 class Navbar extends React.Component {
-  state = {}
+  state = {};
 
-  render(){
+  render() {
     const { toggleDrawer } = this.props;
-    let arrowIcon="/icons/arrowDown.svg";
+    let arrowIcon = "/icons/arrowDown.svg";
 
     return (
-    <nav className="Navbar">
-      {/* The mobile menu. Is only displayed on mobile screen */}
-      <DrawerToggleButton
-        toggleDrawer={toggleDrawer}
-      />{" "}
-      {/* The desktop menu. Is only displayed screens larger then mobile */}
-      <div className="desktop-menu-options">
-        <Link className="nav-link" to="/">
-          Hem
-        </Link>
-        <div className="drop-down">
-          <div className="nav-link" id="intervjuerLink">
-            <Link to="/intervjuer">Intervjuer</Link>
-            <img id="arrow-down" src={arrowIcon} alt="arrow" />
-          </div>
-          <div id="dropDownContent">
+      <nav className="Navbar">
+        {/* The mobile menu. Is only displayed on mobile screen */}
+        <DrawerToggleButton toggleDrawer={toggleDrawer} />{" "}
+        {/* The desktop menu. Is only displayed screens larger then mobile */}
+        <div className="desktop-menu-options">
+          <Link className="nav-link" to="/">
+            Hem
+          </Link>
+          <div className="drop-down">
+            <div className="nav-link" id="intervjuerLink">
+              <Link to="/intervjuer">Intervjuer</Link>
+              {/*<img id="arrow-down" src={arrowIcon} alt="arrow" />*/}
+            </div>
+            {/*<div id="dropDownContent">
             <Link to="/intervjuer">Afrika</Link>
             <Link to="/intervjuer">Asien</Link>
             <Link to="/intervjuer">Europa</Link>
             <Link to="/intervjuer">Nordamerika</Link>
             <Link to="/intervjuer">Oceanien</Link>
+          </div>*/}
           </div>
+          <Link className="nav-link" to="/avtal">
+            Avtal
+          </Link>
+          <Link className="nav-link" to="/inforResa">
+            Inför resan
+          </Link>
+          <Link className="nav-link" to="/tillgodoraknande">
+            Tillgodoräknande
+          </Link>
+          <Link className="nav-link" to="/faq">
+            FAQ
+          </Link>
         </div>
-        <Link className="nav-link" to="/avtal">
-          Avtal
-        </Link>
-        <Link className="nav-link" to="/inforResa">
-          Inför resan
-        </Link>
-        <Link className="nav-link" to="/tillgodoraknande">
-          Tillgodoräknande
-        </Link>
-        <Link className="nav-link" to="/faq">
-          FAQ
-        </Link>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
   }
-};
+}
 
 //export default Navbar;
 export default Navbar;
