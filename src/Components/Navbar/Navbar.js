5d5ea527ca454c518b1c67a34a74e79ace6e2854
Hanna Andersson <hannaandersson010@gmail.com>
2018-12-20 15:53:16 +0100

    cleaning

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,30 +4,29 @@ import "./Navbar.scss";
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
 
 const Navbar = ({ toggleDrawer }) => {
-
   return (
     <nav className="Navbar">
       {/* The mobile menu button is only displayed on mobile screen */}
       <DrawerToggleButton toggleDrawer={toggleDrawer} />
       <div className="desktop-menu-options">
-        <NavLink exact className="nav-link" to="/">
+        <NavLink exact className="navigation-link" to="/">
           Hem
         </NavLink>
-        <div className="drop-down">
-          <div className="nav-link" id="intervjuerLink">
-            <NavLink to="/intervjuer">Intervjuer</NavLink>
-          </div>
-        </div>
-        <NavLink className="nav-link" to="/avtal">
+
+        <NavLink className="navigation-link" to="/intervjuer">
+          Intervjuer
+        </NavLink>
+
+        <NavLink className="navigation-link" to="/avtal">
           Avtal
         </NavLink>
-        <NavLink className="nav-link" to="/inforResa">
+        <NavLink className="navigation-link" to="/inforResa">
           Inför resan
         </NavLink>
-        <NavLink className="nav-link" to="/tillgodoraknande">
+        <NavLink className="navigation-link" to="/tillgodoraknande">
           Tillgodoräknande
         </NavLink>
-        <NavLink className="nav-link" to="/faq">
+        <NavLink className="navigation-link" to="/faq">
           FAQ
         </NavLink>
       </div>
